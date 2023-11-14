c3c62723224df5268defe1ab8febcd0c973433f2
Nicholas L <NLoo1@outlook.com>
2023-11-14 09:34:00 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// Q1
+new Set([1,1,2,2,3,4]) // {1,2,3,4}
+
+//Q2
+[...new Set("referee")].join("") // "ref"
+
+//Q3
+let m = new Map();
+m.set([1,2,3], true);
+m.set([1,2,3], false);
+
+// 0: {Array(3) => true}
+// 1: {Array(3) => false}
+
+//hasDuplicate
+// Write a function called hasDuplicate which accepts an array and returns true or false if that array contains a duplicate
+// hasDuplicate([1,3,2,1]) // true
+// hasDuplicate([1,5,-1,4]) // false
+
+const hasDuplicate = arr => new Set(arr).size !== arr.length
+
+
+// vowelCount
+
+// Write a function called vowelCount which accepts a string and returns a map where the keys are numbers and the values are the count of the vowels in the string.
+
+const vowelCount = str => {
+    const vowelMap = new Map();
+    for(let char of str){
+      let lowerCaseChar = char.toLowerCase()
+      if("aeiou".includes(lowerCaseChar)){
+        if(vowelMap.has(lowerCaseChar)){
+          vowelMap.set(lowerCaseChar, vowelMap.get(lowerCaseChar) + 1);
+        } else {
+          vowelMap.set(lowerCaseChar, 1);
+        }
+      }
+    }
+    return vowelMap;
+  }
